47d3823d973a26b295b64ee3fdb74c46ce44c866
robert <zeng444@163.com>
2021-11-18 15:31:03 +0800

    init 2

--- gateway/config/rpc.php
+++ gateway/config/rpc.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'client' => [
-        'appId' => '460601755',
-        'appSecret' => '915a04c607248c40fe2f116de216dd640fd67fce',
+        'appId' => '100000001',
+        'appSecret' => 'e2d345c607248c40fe2d236de216dd640fd6df3e',
     ],
 ];
--- service/config/voice.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'appId' => 'AwgVgmiJySWQSDxF',
-    'appSecret' => 'AwgVgmiJySWQSDxFwFGs8vJrgWPgDabF',
-    'templateId' => '453662',
-];
